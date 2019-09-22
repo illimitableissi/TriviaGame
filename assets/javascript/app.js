32c4497ea2697225dbb9bdee34e7f32d89836b3b
SOVEREIGN <isimemeni@gmail.com>
2019-09-22 15:22:52 -0400

    added questions to js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+//Questions
+var questions = [{
+    ques: "Whos death pushed Goku to go Super Saiyan for the first time?",
+    ans: ["Gohan", "Piccolo","Tien","Krillin"],
+    correct: "Krillen",
+},
+{
+    ques:"Which fusion only lasts for 30 minutes?",
+    ans: ["Potara Fusion","Fusion Dance", "Namekian Fusion", "Majin Fusion"],
+    correct: "Fushion Dance",
+},
+{
+    ques:"Which universe won the Tournament of Power?",
+    ans: ["Universe 9", "Universe 7", "Universe 5","Universe 8"],
+    correct: "Universe 7"
+},
+{
+    ques:"Who is the most reoccuring Dragon Ball movie villian?",
+    ans: ["Broly", "Cooler", "Bojack", "Janemba"],
+    correct: "Broly"
+},
+{
+    ques:"Which character did Buu not absorb?",
+    ans: ["Gohan", "Gotenks", "Piccolo", "Tien"],
+    correct: "Tien"
+},
+{   ques:"Which new level did Goku reach during the Tournament of Power?",
+    ans: ["Super Saiyan Blue", "Ultra Instinct", "Super Saiyan Rose", "Super Saiyan 4"],
+    correct: "Ultra Instinct"
+},
+{   ques:"How many androids did Cell need to absorb to become Perfect Cell?",
+    ans: ["4", "2", "6", "1"],
+    correct: "2"
+},
+{
+    ques:"Which character defeated Cell?",
+    ans: ["Gohan", "Goku", "Vegeta", "Trunks"],
+    correct: "Gohan"
+},
+{
+    ques:"Which character did Babidi take control of during the Buu saga?",
+    ans: ["Vegeta", "Goku", "Piccolo", "Goten"],
+    correct: "Vegeta"
+},
+{
+    ques:"What was the name of the organization that created all of the androids in Dragon Ball?",
+    ans: ["Capsule Corporation", "Red Ribbon Army", "Ginyu Force", "Cooler's Armored Squadron"],
+    correct: "Red Ribbon Army"
+},
+{    
+    ques:"Which character was able to befriend Majin Buu and turn him good?",
+    ans: ["Gotenks", "Hercule", "Bibidi", "Yamcha"],
+    correct: "Hercule"
+},
+]   
