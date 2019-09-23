a3a2ea717594748200ede63397d8fbc7d16fbecd
SOVEREIGN <isimemeni@gmail.com>
2019-09-22 22:47:20 -0400

    added questions and answers view to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,86 @@
 //Questions
-var questions = [{
-    ques: "Whos death pushed Goku to go Super Saiyan for the first time?",
-    ans: ["Gohan", "Piccolo","Tien","Krillin"],
+var questions = [
+{
+    question: "Whos death pushed Goku to go Super Saiyan for the first time?",
+    answer: ["Gohan", "Piccolo","Tien","Krillin"],
     correct: "Krillen",
 },
 {
-    ques:"Which fusion only lasts for 30 minutes?",
-    ans: ["Potara Fusion","Fusion Dance", "Namekian Fusion", "Majin Fusion"],
+    question:"Which fusion only lasts for 30 minutes?",
+    answer: ["Potara Fusion","Fusion Dance", "Namekian Fusion", "Majin Fusion"],
     correct: "Fushion Dance",
 },
 {
-    ques:"Which universe won the Tournament of Power?",
-    ans: ["Universe 9", "Universe 7", "Universe 5","Universe 8"],
+    question:"Which universe won the Tournament of Power?",
+    answerr: ["Universe 9", "Universe 7", "Universe 5","Universe 8"],
     correct: "Universe 7"
 },
 {
-    ques:"Who is the most reoccuring Dragon Ball movie villian?",
-    ans: ["Broly", "Cooler", "Bojack", "Janemba"],
+    question:"Who is the most reoccuring Dragon Ball movie villian?",
+    answer: ["Broly", "Cooler", "Bojack", "Janemba"],
     correct: "Broly"
 },
 {
-    ques:"Which character did Buu not absorb?",
-    ans: ["Gohan", "Gotenks", "Piccolo", "Tien"],
+    question:"Which character did Buu not absorb?",
+    answer: ["Gohan", "Gotenks", "Piccolo", "Tien"],
     correct: "Tien"
 },
-{   ques:"Which new level did Goku reach during the Tournament of Power?",
-    ans: ["Super Saiyan Blue", "Ultra Instinct", "Super Saiyan Rose", "Super Saiyan 4"],
+{   question:"Which new level did Goku reach during the Tournament of Power?",
+    answer: ["Super Saiyan Blue", "Ultra Instinct", "Super Saiyan Rose", "Super Saiyan 4"],
     correct: "Ultra Instinct"
 },
-{   ques:"How many androids did Cell need to absorb to become Perfect Cell?",
-    ans: ["4", "2", "6", "1"],
+{   question:"How many androids did Cell need to absorb to become Perfect Cell?",
+    answer: ["4", "2", "6", "1"],
     correct: "2"
 },
 {
-    ques:"Which character defeated Cell?",
-    ans: ["Gohan", "Goku", "Vegeta", "Trunks"],
+    question:"Which character defeated Cell?",
+    answer: ["Gohan", "Goku", "Vegeta", "Trunks"],
     correct: "Gohan"
 },
 {
-    ques:"Which character did Babidi take control of during the Buu saga?",
-    ans: ["Vegeta", "Goku", "Piccolo", "Goten"],
+    question:"Which character did Babidi take control of during the Buu saga?",
+    answer: ["Vegeta", "Goku", "Piccolo", "Goten"],
     correct: "Vegeta"
 },
 {
-    ques:"What was the name of the organization that created all of the androids in Dragon Ball?",
-    ans: ["Capsule Corporation", "Red Ribbon Army", "Ginyu Force", "Cooler's Armored Squadron"],
+    question:"What was the name of the organization that created all of the androids in Dragon Ball?",
+    answer: ["Capsule Corporation", "Red Ribbon Army", "Ginyu Force", "Cooler's Armored Squadron"],
     correct: "Red Ribbon Army"
 },
 {    
-    ques:"Which character was able to befriend Majin Buu and turn him good?",
-    ans: ["Gotenks", "Hercule", "Bibidi", "Yamcha"],
+    question:"Which character was able to befriend Majin Buu and turn him good?",
+    answer: ["Gotenks", "Hercule", "Bibidi", "Yamcha"],
     correct: "Hercule"
 },
 ]   
-
+// Initial Values
 var counter = 60;
+var currentQuestion = 0;
 var correct = 0;
 var incorrect = 0;
 var timer;
 
 var gameStart = $("#startButton").on('click', function(){
     $('#startButton').hide();
-    $('#actualGame').show();
 });
 
 //loops through questions
 function loadQuestions () {
-    $('#time-remain').html(counter);
-for (var q = 0; q < 11; q++) {
-    $('#questions').append('<h5>' + questions[q].ques + '</h5>' + '<hr />');
-    console.log(questions[q].ques)
-    for (var a = 0; a < 4; a++) {
-    $('#answers').append('<label for =' + questions[q].ans[a] + '</label>')
-    console.log(questions[q].ans[a])
- }
-}
+    var questionOne = questions[currentQuestion].question
+    var answerOne = questions[currentQuestion].answer
+    $('#timer').html('Timer:' + counter);
+    $('#game').html('<h4>' + questionOne + '</h4>'); 
+    $('#game').append(loadChoices(answerOne))
 }
 
-loadQuestions()
+function loadChoices (answerOne) {
+    var result = '';
+    for (var i = 0; i < 4; i++) {
+        console.log(answerOne[i])
+        result += ('<p class=ans' + ' ' + 'data-answer=' + answerOne[i] +  '> ' + answerOne[i] + ' ' + '</p>');  
+    }
+    return result;
+}
+
+loadQuestions();
