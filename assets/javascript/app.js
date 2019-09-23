7685ed31aff6fc93196d1ed4579b6cba2dfad213
SOVEREIGN <isimemeni@gmail.com>
2019-09-23 08:14:35 -0400

    added full game functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,59 +2,71 @@
 var questions = [
 {
     question: "Whos death pushed Goku to go Super Saiyan for the first time?",
-    answer: ["Gohan", "Piccolo","Tien","Krillin"],
+    answer: ["Gohan", "Piccolo", "Tien", "Krillin"],
     correct: "Krillin",
     image: './assets/images/krillin.gif',
 },
 {
     question:"Which fusion only lasts for 30 minutes?",
-    answer: ["Potara Fusion","Fusion Dance", "Namekian Fusion", "Majin Fusion"],
-    correct: "Fusion Dance",
+    answer: ["Potara-Fusion", "Fusion-Dance", "Namekian-Fusion", "Majin-Fusion"],
+    correct: "Fusion-Dance",
+    image: './assets/images/fusion.gif',
 },
 {
     question:"Which universe won the Tournament of Power?",
-    answer: ["Universe 9", "Universe 7", "Universe 5","Universe 8"],
-    correct: "Universe 7"
+    answer: ["Universe-9", "Universe-7", "Universe-5", "Universe-8"],
+    correct: "Universe-7",
+    image: './assets/images/universe7.png',
 },
 {
     question:"Who is the most reoccuring Dragon Ball movie villian?",
     answer: ["Broly", "Cooler", "Bojack", "Janemba"],
-    correct: "Broly"
+    correct: "Broly",
+    image: './assets/images/broly.gif',
 },
 {
     question:"Which character did Buu not absorb?",
     answer: ["Gohan", "Gotenks", "Piccolo", "Tien"],
-    correct: "Tien"
+    correct: "Tien",
+    image: './assets/images/tien.jpg',
 },
-{   question:"Which new level did Goku reach during the Tournament of Power?",
-    answer: ["Super Saiyan Blue", "Ultra Instinct", "Super Saiyan Rose", "Super Saiyan 4"],
-    correct: "Ultra Instinct"
+{   
+    question:"Which new level did Goku reach during the Tournament of Power?",
+    answer: ["Super Saiyan Blue", "Ultra-Instinct", "Super Saiyan Rose", "Super Saiyan 4"],
+    correct: "Ultra-Instinct",
+    image: './assets/images/ultrainstinct.gif',
 },
 {   question:"How many androids did Cell need to absorb to become Perfect Cell?",
     answer: ["4", "2", "6", "1"],
-    correct: "2"
+    correct: "2",
+    image: './assets/images/androids.gif',
 },
 {
     question:"Which character defeated Cell?",
     answer: ["Gohan", "Goku", "Vegeta", "Trunks"],
-    correct: "Gohan"
+    correct: "Gohan",
+    image: './assets/images/gohan.gif',
 },
 {
     question:"Which character did Babidi take control of during the Buu saga?",
     answer: ["Vegeta", "Goku", "Piccolo", "Goten"],
-    correct: "Vegeta"
+    correct: "Vegeta",
+    image: './assets/images/majinvegeta.gif',
 },
 {
     question:"What was the name of the organization that created all of the androids in Dragon Ball?",
-    answer: ["Capsule Corporation", "Red Ribbon Army", "Ginyu Force", "Cooler's Armored Squadron"],
-    correct: "Red Ribbon Army"
+    answer: ["Capsule-Corporation", "Red-Ribbon-Army", "Ginyu-Force", "Cooler's Armored Squadron"],
+    correct: "Red-Ribbon-Army",
+    image: './assets/images/redribbon.jpg',
 },
 {    
     question:"Which character was able to befriend Majin Buu and turn him good?",
     answer: ["Gotenks", "Hercule", "Bibidi", "Yamcha"],
-    correct: "Hercule"
+    correct: "Hercule",
+    image: './assets/images/hercule.gif',
 },
 ]   
+
 // Initial Values
 var counter = 15;
 var currentQuestion = 0;
@@ -67,7 +79,7 @@ $('#game').hide()
 $('#timer').hide()
 
 
-
+//Loads result display if there are no more questions
 function nextQuestion(){
     var questionOver = (questions.length -1) === currentQuestion
     if (questionOver) {
@@ -82,7 +94,9 @@ function nextQuestion(){
 function timeUp () {
     clearInterval(timer);
     incorrect++;
-    nextQuestion();
+    $('#game').html('<h5>' + 'Time is up! The correct answer was ' + questions[currentQuestion].correct + '</h5>')
+    $('#game').append('<img src=' + questions[currentQuestion].image + '>')
+    setTimeout(nextQuestion, 3 * 1000);
 }
 //Function that controls countdown timer
 function countDown() {
@@ -91,8 +105,8 @@ function countDown() {
     if (counter === 0){
         timeUp()
     }
-
 }
+
 //Function that starts game
 function gameStart () { 
     $("#startButton").on('click', function(){
@@ -122,19 +136,23 @@ function loadChoices (answerOne) {
     return result;
 }
 
-//Function determines if clicked answer is correct ot incorrect
+//Function determines if clicked answer is correct or incorrect
 $(document).on('click', '.ans', function (){
     clearInterval(timer)
     var selectedAnswer = $(this).attr('data-answer');
-    var correctAnswer = questions[currentQuestion].correct;
+    var correctAnswer = questions[currentQuestion].correct
+    var correctImage = questions[currentQuestion].image
 
     if (correctAnswer === selectedAnswer) {
         correct++
-        imageShow();
+        $('#game').html('<h3>' + 'Correct!' + '</h3>')
+        $('#game').append('<img src=' + correctImage + '>')
         setTimeout(nextQuestion, 3 * 1000);
     } else{
         incorrect++
-        nextQuestion();
+        $('#game').html('<h5>' + 'Incorrect! The correct answer was ' + correctAnswer + '</h5>')
+        $('#game').append('<img src=' + correctImage + '>')
+        setTimeout(nextQuestion, 3 * 1000)
     }
 });
 
@@ -156,10 +174,5 @@ $(document).on('click', '#reset', function (){
     loadQuestions();
 
 });
-
-function imageShow() {
-    $('#game').html('<h3>' + 'Correct' + '</h3>')
-    $('#game').append(questions[currentQuestion].image)
-}
 
 gameStart();
